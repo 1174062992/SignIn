191789733834d0dda132ea6db9bc33021ab3b8d6
1174062992 <1174062992>
2018-03-31 16:19:31 +0800

    Initial Commit

---
+++ 签到/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  签到
+//
+//  Created by 曹记 on 2018/3/31.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 签到/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  签到
+//
+//  Created by 曹记 on 2018/3/31.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
